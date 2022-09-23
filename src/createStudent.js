fe15a58a13724c6102500b4d4eb82fbb398bddd9
cunhavcs <cunha.vcs@gmail.com>
2022-09-23 14:28:01 -0300

    Implementa a função "createStudent" (Requisito 05)

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedback = () => 'Eita pessoa boa!';
+
+const createStudent = (nome) => {
+  const objeto = { name: nome, feedback };
+  return objeto;
+};
 
 module.exports = createStudent;
